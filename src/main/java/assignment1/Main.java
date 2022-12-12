d3ababb5da3eb2c38aab4391740bca7929d38b73
Alexia <alexia.gross@insa-rouen.fr>
2022-12-12 13:46:19 +0100

    First and Second assignment

--- src/main/java/assignment1/Main.java
+++ src/main/java/assignment1/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(Calculator.multiply(80, 70));
+        System.out.println(Calculator.divide(50, 20));
         System.out.println(Calculator.sum(80, 70));
+        System.out.println(Calculator.substract(80, 70));
     }
 }
--- src/test/java/assignment1/CalculatorTest.java
+++ src/test/java/assignment1/CalculatorTest.java
@@ -6,11 +6,10 @@ import org.junit.jupiter.api.function.Executable;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    public Calculator calc = new Calculator();
 
     @Test
     void multiply() {
-        Calculator calc = new Calculator();
-
         assertEquals(2, calc.multiply(2,1));
         assertEquals(0, calc.multiply(0,70));
     }
@@ -20,9 +19,18 @@ class CalculatorTest {
         assertThrows(ArithmeticException.class, new Executable() {
             @Override
             public void execute() throws Throwable {
-                Calculator calc = new Calculator();
                 calc.divide(3,0);
             }
         });
+    }
+
+    @Test
+    void sum(){
+        assertEquals(4, calc.sum(2, 2));
+    }
+
+    @Test
+    void substract(){
+        assertEquals(8, calc.substract(10, 2));
     }
 }
